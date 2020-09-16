86a6e71fa41fdd105e592aa169a9247fc0db6b9d
wr786 <wr786@pku.edu.cn>
2020-09-16 18:00:09 +0800

    release Ver.

--- wcc.py
+++ wcc.py
@@ -17,12 +17,12 @@ def init():
 	raise '[ERROR] 请检查同目录下是否存在data/infoText!'
 
 def read_op():
-	op = input("[INFO] 请选择操作(1.搜索课程类别 2.搜索类别下课程)\n>>> ")
+	op = input("[INFO] 请选择操作(1.搜索课程类别 2.搜索类别下课程 3.搜索您的所有课程类别)\n>>> ")
 	opInt = 0
 	try:
 		opInt = int(op)
-		if opInt < 1 or opInt > 2:
-			raise '[ERROR] 请输入1|2！'
+		if opInt < 1 or opInt > 3:
+			raise '[ERROR] 请输入1|2|3！'
 	except:
 		raise '[ERROR] 请输入正确的数字!'
 	if opInt == 1:
@@ -31,13 +31,20 @@ def read_op():
 			if className in areaOfClass.keys():
 				print(f'======================================= {className}: {areaOfClass[className]}')
 			else:
-				print(f'{className}并非通选课，或者课程名称有误！如果你确定{className}是通选课，欢迎发表issue！')
+				print(f'{className}并非通选课.')
 	elif opInt == 2:
 		areaID = input("[INFO] 请输入领域名称(A|B|C|D|E|F):\n>>> ")
 		if areaID in ("A", "B", "C", "D", "E", "F"):
 			for className in classOfArea[areaID]:
 				print(className)
 			print(f"[INFO] 以上即为分类{areaID}的所有课程.")
+	elif opInt == 3:
+		with open('classes.csv', 'r') as f:
+			for className in f.read().splitlines():
+				if className in areaOfClass.keys():
+					print(f'======================================= {className}: {areaOfClass[className]}')
+				else:
+					print(f'{className}并非通选课.')
 
 if __name__ == '__main__':
 	init()
